0d16ea2aa8ceb4205083ec865f22cbe7ce8ab892
Jamie Ahale <jamie.w.ahale@gmail.com>
2023-10-18 19:45:18 +1100

    Add likes table

--- db/schema.rb
+++ db/schema.rb
@@ -44,26 +44,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_232409) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "tweet_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tweet_id"], name: "index_comments_on_tweet_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "tweet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "tweets", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tweets_on_user_id"
@@ -87,7 +78,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_232409) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "tweets"
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "tweets"
-  add_foreign_key "likes", "users"
   add_foreign_key "tweets", "users"
 end
